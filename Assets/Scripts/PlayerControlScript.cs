84c13867af3341cc1abcdc52c6ac6545d2f3dbc6
RookieIndieDev <vigneshninja911@gmail.com>
2016-08-12 19:46:29 +0530

    -Added Nuclear throne style lighting -Changed Player movement speed

--- Assets/Scripts/PlayerControlScript.cs
+++ Assets/Scripts/PlayerControlScript.cs
@@ -5,7 +5,11 @@ public class PlayerControlScript : MonoBehaviour
 {
 
 	Rigidbody2D PlayerRigidbody;
-	float Speed = 2.5f;
+	float Speed = 3.5f;
+	float xOffset = 3.3f;
+	float yOffset = 0.49f;
+	Vector3 ScreenSpaceVector;
+	public static Vector4 PlayerPosition;
 	void Start () 
 	{
 		PlayerRigidbody = gameObject.GetComponent<Rigidbody2D> ();
@@ -19,120 +23,128 @@ public class PlayerControlScript : MonoBehaviour
 		if (Input.GetKey ("w")) 
 		{
 			PlayerRigidbody.velocity = Vector2.up * Speed;
-			AnimControlleScript.IsFacingBackward = true;
-			AnimControlleScript.IsFacingForward = false;
-			AnimControlleScript.IsFacingLeft = false;
-			AnimControlleScript.IsFacingRight = false;
-			AnimControlleScript.IsPlayerAttackingUp = false;
-			AnimControlleScript.IsPlayerAttackingDown = false;
-			AnimControlleScript.IsPlayerAttackingLeft = false;
-			AnimControlleScript.IsPlayerAttackingRight = false;
+			AnimControllerScript.IsFacingBackward = true;
+			AnimControllerScript.IsFacingForward = false;
+			AnimControllerScript.IsFacingLeft = false;
+			AnimControllerScript.IsFacingRight = false;
+			AnimControllerScript.IsPlayerAttackingUp = false;
+			AnimControllerScript.IsPlayerAttackingDown = false;
+			AnimControllerScript.IsPlayerAttackingLeft = false;
+			AnimControllerScript.IsPlayerAttackingRight = false;
 		}
 
 		if (Input.GetKey ("s")) 
 		{
 			PlayerRigidbody.velocity = Vector2.down * Speed;
-			AnimControlleScript.IsFacingForward = true;
-			AnimControlleScript.IsFacingBackward = false;
-			AnimControlleScript.IsFacingLeft = false;
-		    AnimControlleScript.IsFacingRight = false;
-			AnimControlleScript.IsPlayerAttackingUp = false;
-			AnimControlleScript.IsPlayerAttackingDown = false;
-			AnimControlleScript.IsPlayerAttackingLeft = false;
-			AnimControlleScript.IsPlayerAttackingRight = false;
+			AnimControllerScript.IsFacingForward = true;
+			AnimControllerScript.IsFacingBackward = false;
+			AnimControllerScript.IsFacingLeft = false;
+		    AnimControllerScript.IsFacingRight = false;
+			AnimControllerScript.IsPlayerAttackingUp = false;
+			AnimControllerScript.IsPlayerAttackingDown = false;
+			AnimControllerScript.IsPlayerAttackingLeft = false;
+			AnimControllerScript.IsPlayerAttackingRight = false;
 		}
 
 		if (Input.GetKey ("a")) 
 		{
 			PlayerRigidbody.velocity = Vector2.left * Speed;
-			AnimControlleScript.IsFacingLeft = true;
-			AnimControlleScript.IsFacingBackward = false;
-			AnimControlleScript.IsFacingForward = false;
-			AnimControlleScript.IsFacingRight = false;
-			AnimControlleScript.IsPlayerAttackingUp = false;
-			AnimControlleScript.IsPlayerAttackingDown = false;
-			AnimControlleScript.IsPlayerAttackingLeft = false;
-			AnimControlleScript.IsPlayerAttackingRight = false;
+			AnimControllerScript.IsFacingLeft = true;
+			AnimControllerScript.IsFacingBackward = false;
+			AnimControllerScript.IsFacingForward = false;
+			AnimControllerScript.IsFacingRight = false;
+			AnimControllerScript.IsPlayerAttackingUp = false;
+			AnimControllerScript.IsPlayerAttackingDown = false;
+			AnimControllerScript.IsPlayerAttackingLeft = false;
+			AnimControllerScript.IsPlayerAttackingRight = false;
 		}
 
 		if (Input.GetKey ("d"))
 		{
 			PlayerRigidbody.velocity = Vector2.right * Speed;
-			AnimControlleScript.IsFacingRight = true;
-			AnimControlleScript.IsFacingBackward = false;
-			AnimControlleScript.IsFacingForward = false;
-			AnimControlleScript.IsFacingLeft = false;
-			AnimControlleScript.IsPlayerAttackingUp = false;
-			AnimControlleScript.IsPlayerAttackingDown = false;
-			AnimControlleScript.IsPlayerAttackingLeft = false;
-			AnimControlleScript.IsPlayerAttackingRight = false;
+			AnimControllerScript.IsFacingRight = true;
+			AnimControllerScript.IsFacingBackward = false;
+			AnimControllerScript.IsFacingForward = false;
+			AnimControllerScript.IsFacingLeft = false;
+			AnimControllerScript.IsPlayerAttackingUp = false;
+			AnimControllerScript.IsPlayerAttackingDown = false;
+			AnimControllerScript.IsPlayerAttackingLeft = false;
+			AnimControllerScript.IsPlayerAttackingRight = false;
 		}
 
 		if(Input.anyKey != true)
 		{
-			AnimControlleScript.IsPlayerIdle = true;
-			AnimControlleScript.IsFacingRight = false;
-			AnimControlleScript.IsFacingBackward = false;
-			AnimControlleScript.IsFacingForward = false;
-			AnimControlleScript.IsFacingLeft = false;
-			AnimControlleScript.IsPlayerAttackingUp = false;
-			AnimControlleScript.IsPlayerAttackingDown = false;
-			AnimControlleScript.IsPlayerAttackingLeft = false;
-			AnimControlleScript.IsPlayerAttackingRight = false;
+			AnimControllerScript.IsPlayerIdle = true;
+			AnimControllerScript.IsFacingRight = false;
+			AnimControllerScript.IsFacingBackward = false;
+			AnimControllerScript.IsFacingForward = false;
+			AnimControllerScript.IsFacingLeft = false;
+			AnimControllerScript.IsPlayerAttackingUp = false;
+			AnimControllerScript.IsPlayerAttackingDown = false;
+			AnimControllerScript.IsPlayerAttackingLeft = false;
+			AnimControllerScript.IsPlayerAttackingRight = false;
 		}
 
 		//Attack Handling
 
 		if (Input.GetKey (KeyCode.UpArrow))
 		{
-			AnimControlleScript.IsPlayerIdle = false;
-			AnimControlleScript.IsFacingRight = false;
-			AnimControlleScript.IsFacingBackward = false;
-			AnimControlleScript.IsFacingForward = false;
-			AnimControlleScript.IsFacingLeft = false;
-			AnimControlleScript.IsPlayerAttackingUp = true;
-			AnimControlleScript.IsPlayerAttackingDown = false;
-			AnimControlleScript.IsPlayerAttackingLeft = false;
-			AnimControlleScript.IsPlayerAttackingRight = false;
+			AnimControllerScript.IsPlayerIdle = false;
+			AnimControllerScript.IsFacingRight = false;
+			AnimControllerScript.IsFacingBackward = false;
+			AnimControllerScript.IsFacingForward = false;
+			AnimControllerScript.IsFacingLeft = false;
+			AnimControllerScript.IsPlayerAttackingUp = true;
+			AnimControllerScript.IsPlayerAttackingDown = false;
+			AnimControllerScript.IsPlayerAttackingLeft = false;
+			AnimControllerScript.IsPlayerAttackingRight = false;
 		}
 
 		if (Input.GetKey (KeyCode.DownArrow)) 
 		{
-			AnimControlleScript.IsPlayerIdle = false;
-			AnimControlleScript.IsFacingRight = false;
-			AnimControlleScript.IsFacingBackward = false;
-			AnimControlleScript.IsFacingForward = false;
-			AnimControlleScript.IsFacingLeft = false;
-			AnimControlleScript.IsPlayerAttackingUp = false;
-			AnimControlleScript.IsPlayerAttackingDown = true;
-			AnimControlleScript.IsPlayerAttackingLeft = false;
-			AnimControlleScript.IsPlayerAttackingRight = false;
+			AnimControllerScript.IsPlayerIdle = false;
+			AnimControllerScript.IsFacingRight = false;
+			AnimControllerScript.IsFacingBackward = false;
+			AnimControllerScript.IsFacingForward = false;
+			AnimControllerScript.IsFacingLeft = false;
+			AnimControllerScript.IsPlayerAttackingUp = false;
+			AnimControllerScript.IsPlayerAttackingDown = true;
+			AnimControllerScript.IsPlayerAttackingLeft = false;
+			AnimControllerScript.IsPlayerAttackingRight = false;
 		}
 
 		if (Input.GetKey (KeyCode.LeftArrow)) 
 		{
-			AnimControlleScript.IsPlayerIdle = false;
-			AnimControlleScript.IsFacingRight = false;
-			AnimControlleScript.IsFacingBackward = false;
-			AnimControlleScript.IsFacingForward = false;
-			AnimControlleScript.IsFacingLeft = false;
-			AnimControlleScript.IsPlayerAttackingUp = false;
-			AnimControlleScript.IsPlayerAttackingDown = false;
-			AnimControlleScript.IsPlayerAttackingLeft = true;
-			AnimControlleScript.IsPlayerAttackingRight = false;
+			AnimControllerScript.IsPlayerIdle = false;
+			AnimControllerScript.IsFacingRight = false;
+			AnimControllerScript.IsFacingBackward = false;
+			AnimControllerScript.IsFacingForward = false;
+			AnimControllerScript.IsFacingLeft = false;
+			AnimControllerScript.IsPlayerAttackingUp = false;
+			AnimControllerScript.IsPlayerAttackingDown = false;
+			AnimControllerScript.IsPlayerAttackingLeft = true;
+			AnimControllerScript.IsPlayerAttackingRight = false;
 		}
 
 		if (Input.GetKey (KeyCode.RightArrow)) 
 		{
-			AnimControlleScript.IsPlayerIdle = false;
-			AnimControlleScript.IsFacingRight = false;
-			AnimControlleScript.IsFacingBackward = false;
-			AnimControlleScript.IsFacingForward = false;
-			AnimControlleScript.IsFacingLeft = false;
-			AnimControlleScript.IsPlayerAttackingUp = false;
-			AnimControlleScript.IsPlayerAttackingDown = false;
-			AnimControlleScript.IsPlayerAttackingLeft = false;
-			AnimControlleScript.IsPlayerAttackingRight = true;
+			AnimControllerScript.IsPlayerIdle = false;
+			AnimControllerScript.IsFacingRight = false;
+			AnimControllerScript.IsFacingBackward = false;
+			AnimControllerScript.IsFacingForward = false;
+			AnimControllerScript.IsFacingLeft = false;
+			AnimControllerScript.IsPlayerAttackingUp = false;
+			AnimControllerScript.IsPlayerAttackingDown = false;
+			AnimControllerScript.IsPlayerAttackingLeft = false;
+			AnimControllerScript.IsPlayerAttackingRight = true;
 		}
+
+		//Convert World Space to Screen Space
+		ScreenSpaceVector.Set(gameObject.transform.position.x + xOffset + Random.Range(0f,0.1f), -gameObject.transform.position.y + yOffset + Random.Range(0f,0.1f), gameObject.transform.position.z);
+		ScreenSpaceVector = Camera.main.WorldToScreenPoint(ScreenSpaceVector);
+
+		// Set the Position to be sent to the Shader
+		PlayerPosition.Set(ScreenSpaceVector.x, ScreenSpaceVector.y, ScreenSpaceVector.z, 0);
+
 	}
 }
